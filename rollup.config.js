2d20d18ca5888a3a9efb0caf5187e8b2e669bb53
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2020-11-18 21:57:33 +0000

    nú ættu skripturnar að vera komnar

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,27 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
 module.exports = {
-  input: './src/index.js',
-  output: {
-    file: './dist/bundle.js',
-    format: 'iife',
-    sourcemap: true,
-  },
-  plugins: [
-    babel({
-      sourcemaps: true,
-      presets: [['@babel/preset-env',
-        { targets: '> 0.25%, not dead' }]],
-    }),
-  ],
+    input: './src/index.js',
+    output: {
+        file: './dist/bundle.js',
+        format: 'iife',
+        sourcemap: true,
+    },
+    plugins: [
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**',
+            sourcemaps: true,
+            presets: [['@babel/preset-env',
+               { useBuiltIns: 'usage',
+                 corejs: 2,
+                 targets: '> 0.25%, not dead' }]],
+        }),
+        resolve(),
+        commonjs(),
+        terser(),
+    ],
 };
